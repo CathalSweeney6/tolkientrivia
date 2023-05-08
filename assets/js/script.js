c0d9aa9d0610b12171ce8508326ac0e2a7bfea5b
CathalSweeney6 <>
2023-05-08 17:17:12 +0000

    Added css and javascript elements

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        question: "The Dúnedain were descendants of which race of people of the Second-Age?",
+        answers: [
+            { text "The Elves of Lindon", correct: false},
+            { text "The Númenóreans", correct: true},
+            { text "The Dwarves of Khazad-dûm", correct: false},
+            { text "The Orcs of Mordor", correct: false},
+        ]
+    },
+    {
+        question: "Which Orc lieutenant shared his name with the Lord of the Balrogs?",
+        answers: [
+            { text "Grishnákh", correct: false},
+            { text "Shagrat", correct: false},
+            { text "Snaga", correct: false},
+            { text "Gothmog", correct: true},
+        ]
+    },
+    {
+        question: "What was the name of the Golden Hall that stood upon the green terrace of Edoras, capital city of the kingdom of Rohan?",
+        answers: [
+            { text "Grishnákh", correct: false},
+            { text "Shagrat", correct: false},
+            { text "Snaga", correct: false},
+            { text "Gothmog", correct: true},
+        ]
+    },
+
+]
