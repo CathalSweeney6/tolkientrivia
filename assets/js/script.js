8f9770157d9be548b4a0c2aeb0a72023d51a81a5
Cathal Sweeney <cjsweeney6@gmail.com>
2023-05-22 17:11:11 +0000

    revert for another test on error

--- assets/js/script.js
+++ assets/js/script.js
@@ -390,7 +390,7 @@ const saveYourScore = document.getElementById("saveYourScore");
 const scoreText = document.getElementById("score");
 const latestScore = localStorage.getItem("latestScore");
 const finalScore = document.getElementById("finalScore");
-if (latestScore != null) {
+if (latestScore) {
     finalScore.innerText = latestScore;
 }
 const max_highscorelist = 6;
