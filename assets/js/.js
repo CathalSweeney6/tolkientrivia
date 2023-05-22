481ae68ecc37bb27cc073ca7e3521936e3f47fe2
Cathal Sweeney <cjsweeney6@gmail.com>
2023-05-22 19:49:29 +0000

    rephrase fix on error

---
+++ assets/js/.js
@@ -0,0 +1,11 @@
+// Consts for leaderboard display. 
+
+const leaderboard = document.getElementById("leaderboard");
+highscorelist = JSON.parse(localStorage.getItem("highscorelist")) || [];
+// leaderboard contents. 
+
+leaderboard.innerHTML = highscorelist.map(score => {
+    return `<li class="leaderboard">${score.name} - ${score.score} </li>`;
+  })
+  .join("");
+
