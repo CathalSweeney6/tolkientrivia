1ed2cd0c9099458fa6ac06b4b604222b97637451
Cathal Sweeney <cjsweeney6@gmail.com>
2023-05-23 14:49:48 +0000

    test for fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -389,8 +389,6 @@ let shuffledQuestions, currentQuestionIndex;
 let score = 0;
 const username = document.getElementById("username");
 const saveYourScore = document.getElementById("saveYourScore");
-const scoreText = document.getElementById("score");
-const latestScore = localStorage.getItem("latestScore");
 const finalScore = document.getElementById("finalScore");
 function latestScore() {
 finalScore.innerText = latestScore;
